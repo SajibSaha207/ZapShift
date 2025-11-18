441f50e5f15d212bd364e75823f65f8dd8b1a19f
SajibSaha207 <sajibsaha.207@gmail.com>
2025-11-19 01:23:49 +0600

    added footer nad navbar

---
+++ src/LayOut/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/NavBar/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+    
+        </div>
+    );
+};
+
+export default RootLayout;
