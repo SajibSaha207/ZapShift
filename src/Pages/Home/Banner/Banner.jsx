ba3664fcce215b7ec36cbe3618b165f61fbd475c
SajibSaha207 <sajibsaha.207@gmail.com>
2025-11-19 17:49:37 +0600

    added button

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import bannerImg1 from '../../../assets/banner/banner1.png';
+import bannerImg2 from '../../../assets/banner/banner2.png';
+import bannerImg3 from '../../../assets/banner/banner3.png';
+import { BsArrowUpRightCircleFill } from 'react-icons/bs';
+
+const Banner = () => {
+    return (
+        <Carousel 
+        // autoPlay={true}
+        // infiniteLoop={true}
+        // interval={2000}
+        >
+        <div className='relative  '>
+            <img src={bannerImg1} alt="" />
+        <div className='absolute bottom-10 left-10 flex items-center gap-2 '>
+            <button className=' bg-primary rounded-full w-[150px] h-[40px] font-bold text-black   ' >Track Your Parcel
+                 </button>
+                  <BsArrowUpRightCircleFill className='text-3xl' /> 
+            <button className='border rounded w-[120px] h-[30px] bg-white font-bold  '>Be A Rider</button>
+
+        </div>
+        </div>
+        <div>
+            <img src={bannerImg2} alt="" />
+           
+        </div>
+        <div>
+            <img src={bannerImg3} alt="" />
+             
+        </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
