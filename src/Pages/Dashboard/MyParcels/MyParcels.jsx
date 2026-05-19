d5bfd222e887bb9543b7d501bd5b89b39184d448
SajibSaha207 <sajibsaha.207@gmail.com>
2026-05-19 14:44:52 +0600

    added parcel delete function

--- src/Pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,18 +2,53 @@ import React from 'react';
 import UseAuth from '../../../hocks/UseAuth';
 import useAxiosSecure from '../../../hocks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import { FiEdit } from 'react-icons/fi';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { FaTrash } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
     const { user } = UseAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels =[]} = useQuery({
+    const { data: parcels =[], refetch} = useQuery({
         queryKey: ['MyParcels', user?.email],
         queryFn: async () =>{
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
         }
     })
+
+const handleParcelDelete = id =>{
+    Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    axiosSecure.delete(`/parcels/${id}`)
+    .then(res =>{
+        console.log(res.data)
+
+        if (res.data.deletedCount){
+            refetch()
+                Swal.fire({
+    title: "Deleted!",
+    text: "Your parcel has been deleted.",
+    icon: "success"
+  });
+        }
+    })
+
+  }
+});
+}
+
     return (
         <div>
             <h2>All of my parcels: {parcels.length}</h2>
@@ -38,6 +73,11 @@ const MyParcels = () => {
         <td>{parcel.parcelName}</td>
         <td>{parcel.cost}</td>
         <td>Blue</td>
+        <td>
+            <button className='btn btn-square hover:bg-primary'><FaMagnifyingGlass></FaMagnifyingGlass></button>
+            <button  className='btn btn-square hover:bg-primary mx-2'><FiEdit></FiEdit></button>
+            <button onClick={()=> handleParcelDelete(parcel._id)} className='btn btn-square hover:bg-primary'><FaTrash></FaTrash></button>
+        </td>
       </tr>
             )
         }
