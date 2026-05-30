cf507dcd233a6c6a7073e9ac3fce188c08904813
SajibSaha207 <sajibsaha.207@gmail.com>
2026-05-30 07:50:39 +0600

    added users admin function

--- src/Pages/Dashboard/UsersManagement/UserManagement.jsx
+++ src/Pages/Dashboard/UsersManagement/UserManagement.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import useAxiosSecure from '../../../hocks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import { FaUserShield } from 'react-icons/fa';
+import { FiShieldOff } from 'react-icons/fi';
+import Swal from 'sweetalert2';
 
 const UserManagement = () => {
 
 const axiosSecure  = useAxiosSecure();
 
-const { data: users = []} = useQuery({
+const { refetch, data: users = []} = useQuery({
     queryKey: ['users'],
     queryFn: async()=>{
         const res = await axiosSecure.get('/users');
@@ -14,6 +17,40 @@ const { data: users = []} = useQuery({
     }
 })
 
+const handleMakeUser = user =>{
+    const roleInfo = {role : 'admin'}
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then(res =>{
+        if (res.data.modifiedCount){
+            refetch()
+            Swal.fire({
+                      position: "top-end",
+                      icon: "success",
+                      title: `${user.displayName} marked as an admin`,
+                      showConfirmButton: false,
+                      timer: 1500
+                    });
+        }
+    })
+}
+
+
+const handleRemoveAdmin = user =>{
+    const roleInfo = {role: 'user'}
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then(res =>{
+        if (res.data.modifiedCount){
+            refetch()
+             Swal.fire({
+                      position: "top-end",
+                      icon: "success",
+                      title: `${user.displayName} marked as an admin`,
+                      showConfirmButton: false,
+                      timer: 1500
+                    });
+        }
+    })
+}
     return (
         <div>
             <h3 className="text-4xl">Users : {users.length}</h3>
@@ -27,8 +64,9 @@ const { data: users = []} = useQuery({
         </th>
         <th>Name</th>
         <th>Email</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Role</th>
+        <th>Admin Action</th>
+        <th>Other Action</th>
       </tr>
     </thead>
     <tbody>
@@ -57,9 +95,19 @@ const { data: users = []} = useQuery({
         <td>
          {user.email}
         </td>
-        <td>Purple</td>
+        <td>
+         {user.role}
+        </td>
+          <td>
+            {
+                user.role === 'admin' ? <button onClick={() =>handleRemoveAdmin(user) } className='btn bg-red-400'><FiShieldOff></FiShieldOff></button> : <button onClick={()=> handleMakeUser(user)} className='btn bg-green-400'><FaUserShield></FaUserShield></button>
+            }
+         
+         
+        </td>
+        <td>Admin</td>
         <th>
-          <button class="btn btn-ghost btn-xs">details</button>
+          <button class="btn btn-ghost btn-xs">Actions</button>
         </th>
       </tr>
       )}
